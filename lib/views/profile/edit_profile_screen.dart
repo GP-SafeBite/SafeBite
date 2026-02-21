1fc11ba6039e275711acbeef946e53bdbcf23ad1
HamsaSa <Hamsa_s27@hotmail.com>
2026-02-22 00:27:59 +0300

    Fix get_x import error, merge main screens into Hamsa

--- lib/views/profile/edit_profile_screen.dart
+++ lib/views/profile/edit_profile_screen.dart
@@ -1,6 +1,6 @@
 // lib/views/profile/edit_profile_screen.dart
 import 'package:flutter/material.dart';
-import 'package:get_x/get.dart';
+import 'package:get/get.dart'; // ✅ correct
 import 'package:google_fonts/google_fonts.dart';
 import '../../widgets/custom_button.dart';
 
